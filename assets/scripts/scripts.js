90a1985cf26fc79f3305ea9a24a7111a94c823a7
Lucas Garcia Buixens <Lucasgb@hotmail.com>
2023-07-05 17:28:31 -0300

    mejoras varias

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,1 +1,17 @@
+//copyright year
 
+document.querySelector(".year-copy").innerText = new Date().getFullYear();
+
+
+// let limenu = document.querySelectorAll(".nav-item");
+// for (i=0; limenu.length-1; i++){
+
+// limenu[i].addEventListener("click", function(){
+
+//     document.querySelector("#navbarNav").classList.remove("show");
+
+// })
+// }
+
+
+AOS.init(); 
