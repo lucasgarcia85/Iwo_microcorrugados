83f8786b1da03ceb8977d7642bdd02df7391c2c1
Lucas Garcia Buixens <Lucasgb@hotmail.com>
2023-07-05 19:18:01 -0300

    solucionado menu hamburguesa que se cierre al clickear

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -2,7 +2,14 @@
 
 document.querySelector(".year-copy").innerText = new Date().getFullYear();
 
+// JS para que al clickear en nav items se cierre hamburguesa
 
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarNav')
+const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
 // let limenu = document.querySelectorAll(".nav-item");
 // for (i=0; limenu.length-1; i++){
 
